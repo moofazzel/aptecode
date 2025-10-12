8cf5d0e1c4d9a7f97f78af64281beb8e49944f56
jakia911 <gazijakia911@gmail.com>
2025-10-12 22:15:29 +0600

    build error solved

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ import ContactForm from "@/components/modules/Contact/ContactForm";
 import MapSection from "@/components/modules/Contact/Map";
 import PageBanner from "../../components/shared/PageBanner";
 export default function AboutPage() {
-  const calLink = "gazi-jakia-sultana-jui-ofjboj/30min";
+  const calLink = "gazi-jakia-sultana-jui-ofjboj/30min"; // your link
   return (
     <div>
       <PageBanner title="Contact Aptecode" bgImage="/img/bg-img/bg.png" />
