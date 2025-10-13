9e596e4c949e9e45fa3b89983f4cbf58757de7d6
moofazzel <mofazzel444.com>
2025-10-13 21:07:51 +0600

    Refactor About page imports: replaced module index file with direct imports from individual component files for improved clarity and maintainability.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,9 @@
-import {
-  AboutSec,
-  CounterStrip,
-  MarqueeSlider,
-  MissionVision,
-  ProcessSection,
-  TestimonialsSection,
-} from "@/components/modules/About";
+import AboutSec from "@/components/modules/About/AboutSec";
+import CounterStrip from "@/components/modules/About/CounterStrip";
+import MarqueeSlider from "@/components/modules/About/MarqeeSlider";
+import MissionVision from "@/components/modules/About/MissionVision";
+import ProcessSection from "@/components/modules/About/ProcessSection";
+import TestimonialsSection from "@/components/modules/About/TestimonialsSection";
 
 export default function AboutPage() {
   return (
--- src/components/modules/about/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as AboutCta } from "./AboutCta";
-export { default as AboutSec } from "./AboutSec";
-export { default as CounterStrip } from "./CounterStrip";
-export { default as MarqueeSlider } from "./MarqeeSlider";
-export { default as MissionVision } from "./MissionVision";
-export { default as ProcessSection } from "./ProcessSection";
-export { default as TestimonialsSection } from "./TestimonialsSection";
